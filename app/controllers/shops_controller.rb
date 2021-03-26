8d171820d9ae9ac459994140af8b8e0230552cb6
Klaudia Stewart <klaudiastewart@gmail.com>
2021-03-25 21:16:34 -0400

    Remove flavors from shops controller and new view

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,25 +1,26 @@
 class ShopsController < ApplicationController
   def index
-    # binding.pry
     @shops = Shop.all
   end
 
   def show
-    # binding.pry
-    @shop = Shop.find(params[:id])
+    @shop = Shop.find(params[:shop_id])
   end
 
-  def show_flavor
-    binding.pry
-    @show_ice_cream = Flavors.all
-    # @show_ice_cream = Flavors.find(params[:ice_cream_shop_id])
+  def show_flavors
+    # @show_ice_cream = Flavors.all
+    # # @show_ice_cream = Flavors.find(params[:ice_cream_shop_id])
+    # flavor = Flavors.new({
+    #   name: params[:flavor][:name],
+    #   nut_free: params[:flavor][:nut_free],
+    #   calories_per_scoop: params[:flavor][:calories_per_scoop]
+    #   })
   end
 
   def new
   end
 
   def create
-    binding.pry
     shop = Shop.new({
       name: params[:shop][:name],
       has_ice_cream_alternatives: params[:shop][:has_ice_cream_alternatives],
@@ -27,13 +28,14 @@ class ShopsController < ApplicationController
       })
     shop.save
 
-    flavor = Flavors.new({
-      name: params[:flavor][:name],
-      nut_free: params[:flavor][:nut_free],
-      calories_per_scoop: params[:flavor][:calories_per_scoop]
-      })
-
-    flavor.save
+    # flavor = Flavors.new({
+    #   name: params[:flavor][:name],
+    #   nut_free: params[:flavor][:nut_free],
+    #   calories_per_scoop: params[:flavor][:calories_per_scoop]
+    #   })
+    #
+    # flavor.save
     redirect_to '/shops'
+    # redirect_to "/shops/#{params[:id]}/flavors"
   end
 end
