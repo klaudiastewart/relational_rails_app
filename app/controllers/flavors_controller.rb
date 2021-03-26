ad966b6ba30becd89a26a3fad2dbc39119dc8690
Klaudia Stewart <klaudiastewart@gmail.com>
2021-03-25 21:15:53 -0400

    Add new view and create new flavor for flavors table

--- app/controllers/flavors_controller.rb
+++ app/controllers/flavors_controller.rb
@@ -1,15 +1,29 @@
 class FlavorsController < ApplicationController
   def index
-    binding.pry
+    # binding.pry
     @ice_cream_types = Flavors.all
   end
 
-  def show
-    @ice_cream_type = Flavors.find(params[:id])
+  def new
   end
 
-  # def create
-  #   binding.pry
-  #   ice_cream_types = shops.new
-  # end
+  def show
+    binding.pry
+    @ice_cream_type = Flavors.find(params[:flavor_id])
+  end
+
+  def create
+    binding.pry
+    flavor = Flavors.new({
+      name: params[:flavor][:name],
+      nut_free: params[:flavor][:nut_free],
+      calories_per_scoop: params[:flavor][:calories_per_scoop]
+      })
+
+    flavor.save
+
+    redirect_to "/flavors"
+
+    # redirect_to "/shops/#{params[:id]}/flavors"
+  end
 end
