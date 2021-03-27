1d7bc9824138ed9d8025d2d442569719607c6b0d
Klaudia Stewart <klaudiastewart@gmail.com>
2021-03-27 13:14:50 -0400

    Add get uri paths for flavors in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
-# post '/shops', to: 'shops#create'
 
   get '/shops', to: 'shops#index'
   get '/shops/new', to: 'shops#new'
   get '/shops/:id', to: 'shops#show'
   get '/shops/:id/flavors', to: 'shops#pc_index'
   post '/shops', to: 'shops#create'
-  # post '/shops/:shop_id/flavors', to: 'shops#create'
 
-  # get '/flavors', to: 'flavors#index'
+  get '/flavors', to: 'flavors#index'
+  get '/flavors/:flavor_id', to: 'flavors#show'
 
   get '/shops/:id/flavors/new', to: 'flavors#new'
   # get '/shops/:id/flavors/:flavor_id', to: 'flavors#show'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_180549) do
+ActiveRecord::Schema.define(version: 2021_03_27_164519) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "moddatetime"
