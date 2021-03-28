fb0e122f6d9ac64fd000040058ec52d3cd7054c0
Klaudia Stewart <klaudiastewart@gmail.com>
2021-03-27 20:59:52 -0400

    Add feature tests for shop index view

--- spec/features/shops/index_spec.rb
+++ spec/features/shops/index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "shops index page", type: :feature do
   it "can see all shops and created at times" do
-    little_man = Shop.create(name: "Little Man's", created_at: Time.now) #2021-03-27 19:03:12 UTC)
+    little_man = Shop.create(name: "Little Man's", created_at: Time.now)
     sweet_cow = Shop.create(name: "Sweet Cow", created_at: Time.now)
 
     visit "/shops"
@@ -19,5 +19,13 @@ RSpec.describe "shops index page", type: :feature do
 
     click_link "Flavors"
     click_link "Shops"
+  end
+
+  it "can click on a link to the shop" do
+    little_man = Shop.create(name: "Little Man's", created_at: Time.now) #2021-03-27 19:03:12 UTC)
+
+    visit "/shops"
+
+    click_link "Little Man's"
   end
 end
