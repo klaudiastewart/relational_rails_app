a95eae52b90b92683b09aecc316bc27324809822
Klaudia Stewart <klaudiastewart@gmail.com>
2021-03-31 17:36:07 -0400

    Update index spec for feature flavors

--- spec/features/flavors/index_spec.rb
+++ spec/features/flavors/index_spec.rb
@@ -31,7 +31,7 @@ RSpec.describe "flavors index page", type: :feature do
     click_link "#{chocolate.name}"
   end
 
-  it "can click on the edit link for a flavor" do
+  it "can click on the edit link for a flavor and you can edit and it saves" do
     little_man = Shop.create!(name: "Little Man's", created_at: Time.now, has_ice_cream_alternatives: true, google_review_rating: 3)
     chocolate = little_man.flavors.create(name: "Chocolate", nut_free: true, calories_per_scoop: 200)
 
@@ -42,5 +42,16 @@ RSpec.describe "flavors index page", type: :feature do
 
     expect(current_path).to eq("/flavors/#{chocolate.id}/edit")
     expect(page).to have_content("Edit #{chocolate.name}")
+
+    fill_in("flavor[name]", with: "Vanilla")
+    fill_in("flavor[nut_free]", with: false)
+    fill_in("flavor[calories_per_scoop]", with: 500)
+
+    click_button "Update Flavor"
+
+    expect(current_path).to eq("/flavors/#{chocolate.id}")
+    expect(page).to have_content("#{chocolate.name}")
+    expect(page).to have_content("#{chocolate.nut_free}")
+    expect(page).to have_content("#{chocolate.calories_per_scoop}")
   end
 end
