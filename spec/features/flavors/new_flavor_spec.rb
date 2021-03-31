cc28842d1ccf6d27c009d2fda18466c7d85b7297
Klaudia Stewart <klaudiastewart@gmail.com>
2021-03-31 13:45:20 -0400

    Update new flavors feature test

--- spec/features/flavors/new_flavor_spec.rb
+++ spec/features/flavors/new_flavor_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "flavors index page", type: :feature do
   it "can click on a button link for flavors and shops index" do
-    little_man = Shop.create(name: "Little Man's", created_at: Time.now)
+    little_man = Shop.create!(name: "Little Man's", created_at: Time.now, has_ice_cream_alternatives: true, google_review_rating: 3)
 
     visit "/shops/#{little_man.id}/flavors/new"
 
@@ -10,21 +10,21 @@ RSpec.describe "flavors index page", type: :feature do
     click_link "Shops"
   end
 
-  # it "can click the create flavor button" do
-  #   little_man = Shop.create(name: "Little Man's", created_at: Time.now)
-  #
-  #   visit "/shops/#{little_man.id}/flavors/new"
-  #
-  #   click_button "Create Flavor"
-  # end
+  it "can fill out a form and be redirected to shops index flavors page with new flavor added" do
+    little_man = Shop.create!(name: "Little Man's", created_at: Time.now, has_ice_cream_alternatives: true, google_review_rating: 3)
 
-  it "to have text boxes exist and can be filled in for flavor questions" do
-    little_man = Shop.create(name: "Little Man's", created_at: Time.now)
+    visit "/shops/#{little_man.id}/flavors"
 
-    visit "/shops/#{little_man.id}/flavors/new"
+    click_link "Create New Flavor"
+    expect(current_path).to eq("/shops/#{little_man.id}/flavors/new")
 
     fill_in("name_input", with: "Chocolate")
     fill_in("nut_free", with: "y")
     fill_in("calories", with: 200)
+
+    click_button "Create Flavor"
+
+    expect(current_path).to eq("/shops/#{little_man.id}/flavors")
+    expect(page). to have_content("Chocolate")
   end
 end
